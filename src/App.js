7627f68113488846a9dcf1c52947b2c90f0fc6ed
Camila <camilalobos.2307@gmail.com>
2020-09-29 19:17:31 -0300

    subiendo landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './componentes/Navbar';
+import Jumbotron from './componentes/Jumbotron';
+import Card from './componentes/Card';
+import Footer from './componentes/Footer';
 
 function App() {
+
+  //Array con 4 objetos que representan las 4 cards:
+  const CardInfo =
+    [
+      {
+        img: "https://s.france24.com/media/display/8c13820c-5b0e-11e9-bf90-005056a964fe/w:980/p:16x9/gato.webp",
+        title: "A little Cat",
+        text: "Cats, also called domestic cats (Felis catus), are small, carnivorous (meat-eating) mammals, of the family Felidae. Domestic cats are often called 'house cats' when kept as indoor pets. Cats have been domesticated (tamed) for nearly 10,000 years. Domestic cats are found in shorthair, longhair, and hairless breed."
+      },
+      {
+        img: "https://s.france24.com/media/display/8c13820c-5b0e-11e9-bf90-005056a964fe/w:980/p:16x9/gato.webp",
+        title: "A little Cat",
+        text: "Cats, also called domestic cats (Felis catus), are small, carnivorous (meat-eating) mammals, of the family Felidae. Domestic cats are often called 'house cats' when kept as indoor pets. Cats have been domesticated (tamed) for nearly 10,000 years. Domestic cats are found in shorthair, longhair, and hairless breed."
+      },
+      {
+        img: "https://s.france24.com/media/display/8c13820c-5b0e-11e9-bf90-005056a964fe/w:980/p:16x9/gato.webp",
+        title: "A little Cat",
+        text: "Cats, also called domestic cats (Felis catus), are small, carnivorous (meat-eating) mammals, of the family Felidae. Domestic cats are often called 'house cats' when kept as indoor pets. Cats have been domesticated (tamed) for nearly 10,000 years. Domestic cats are found in shorthair, longhair, and hairless breed."
+      },
+      {
+        img: "https://s.france24.com/media/display/8c13820c-5b0e-11e9-bf90-005056a964fe/w:980/p:16x9/gato.webp",
+        title: "A little Cat",
+        text: "Cats, also called domestic cats (Felis catus), are small, carnivorous (meat-eating) mammals, of the family Felidae. Domestic cats are often called 'house cats' when kept as indoor pets. Cats have been domesticated (tamed) for nearly 10,000 years. Domestic cats are found in shorthair, longhair, and hairless breed."
+      }
+    ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+
+      <div className="container">
+        <div className="row">
+          <Jumbotron />
+        </div>
+
+        <div className="row mt-4">
+          {CardInfo.map(item => {
+            return <div className="col-md-3 col-sm-12"><Card img={item.img} title={item.title} text={item.text} /></div>
+          })}
+        </div>
+
+      </div>
+
+      <Footer />
+
+    </>
   );
 }
 
